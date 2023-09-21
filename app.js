cdaff7e081aeeb7227c92dcdd0b7bee45ab5e288
vinukmathews <66871552+vinukmathews@users.noreply.github.com>
2023-09-21 22:39:01 +0000

    date

--- app.js
+++ app.js
@@ -263,7 +263,7 @@ app.get("/saveaction", authenticateJWT, async function (req, res) {
   var a = req.query.number;
   var b = req.query.email;
 
-  var newvalues = { crn: a 
+  var newvalues = { crn: a, 
            "createdAt": new Date()
                   };
   await database.collection(decoded).insertOne(newvalues, function (err, res) {
